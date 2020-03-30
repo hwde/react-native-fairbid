51ff8def009e6eb54c6966fcf649c118803297cb
Heiko Weber <heiko@wecos.de>
2020-03-30 10:59:36 +0200

    adjust android

--- android/src/main/java/de/wecos/rnfairbid/RNFairbidModule.java
+++ android/src/main/java/de/wecos/rnfairbid/RNFairbidModule.java
@@ -13,7 +13,6 @@ import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.WritableNativeMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEmitter;
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.fyber.FairBid;
 import com.fyber.fairbid.ads.Banner;
 import com.fyber.fairbid.ads.ImpressionData;
@@ -21,11 +20,13 @@ import com.fyber.fairbid.ads.Interstitial;
 import com.fyber.fairbid.ads.Rewarded;
 import com.fyber.fairbid.ads.banner.BannerError;
 import com.fyber.fairbid.ads.banner.BannerListener;
+import com.fyber.fairbid.ads.banner.BannerOptions;
+import com.fyber.fairbid.ads.banner.SupportedCreativeSizes;
 import com.fyber.fairbid.ads.interstitial.InterstitialListener;
 import com.fyber.fairbid.ads.rewarded.RewardedListener;
 import com.fyber.fairbid.user.UserInfo;
 
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class RNFairbidModule extends ReactContextBaseJavaModule {
@@ -34,92 +35,107 @@ public class RNFairbidModule extends ReactContextBaseJavaModule {
 
     private final BannerListener bannerListener = new BannerListener() {
         @Override
-        public void onError(String placementId, BannerError error) {
+        public void onError(@NonNull String placementId, BannerError error) {
             sendReactEvent("bannerDidFailToLoad", "placementId", placementId);
         }
 
         @Override
-        public void onLoad(String placementId) {
+        public void onLoad(@NonNull String placementId) {
             sendReactEvent("bannerDidLoad", "placementId", placementId);
         }
 
         @Override
-        public void onShow(String placementId, ImpressionData impressionData) {
+        public void onShow(@NonNull String placementId, ImpressionData impressionData) {
             sendReactEvent("bannerDidShow", "placementId", placementId);
         }
 
         @Override
-        public void onClick(String placementId) {
+        public void onClick(@NonNull String placementId) {
             sendReactEvent("bannerDidClick", "placementId", placementId);
+        }
+
+        @Override
+        public void onRequestStart(@NonNull String placementId) {
+            sendReactEvent("requestStart", "placementId", placementId);
         }
     };
 
     private final InterstitialListener interstitialListener = new InterstitialListener() {
         @Override
-        public void onShow(String placementId, ImpressionData impressionData) {
+        public void onShow(@NonNull String placementId, ImpressionData impressionData) {
             sendReactEvent("interstitialDidShow", "placementId", placementId);
         }
 
         @Override
-        public void onClick(String placementId) {
+        public void onClick(@NonNull String placementId) {
             sendReactEvent("interstitialDidClick", "placementId", placementId);
         }
 
         @Override
-        public void onHide(String placementId) {
+        public void onHide(@NonNull String placementId) {
             sendReactEvent("interstitialDidDismiss", "placementId", placementId);
         }
 
         @Override
-        public void onShowFailure(String placementId, ImpressionData impressionData) {
+        public void onShowFailure(@NonNull String placementId, ImpressionData impressionData) {
             sendReactEvent("interstitialDidFailToShow", "placementId", placementId);
         }
 
         @Override
-        public void onAvailable(String placementId) {
+        public void onAvailable(@NonNull String placementId) {
             sendReactEvent("interstitialIsAvailable", "placementId", placementId);
         }
 
         @Override
-        public void onUnavailable(String placementId) {
+        public void onUnavailable(@NonNull String placementId) {
             sendReactEvent("interstitialIsUnavailable", "placementId", placementId);
+        }
+        
+        @Override
+        public void onRequestStart(@NonNull String placementId) {
+            sendReactEvent("requestStart", "placementId", placementId);
         }
     };
 
     private final RewardedListener rewardedListener = new RewardedListener() {
         @Override
-        public void onShow(String placementId, ImpressionData impressionData) {
+        public void onShow(@NonNull String placementId, ImpressionData impressionData) {
             sendReactEvent("rewardedDidShow", "placementId", placementId);
         }
 
         @Override
-        public void onClick(String placementId) {
+        public void onClick(@NonNull String placementId) {
             sendReactEvent("rewardedDidClick", "placementId", placementId);
         }
 
         @Override
-        public void onHide(String placementId) {
+        public void onHide(@NonNull String placementId) {
             sendReactEvent("rewardedDidDismiss", "placementId", placementId);
         }
 
         @Override
-        public void onShowFailure(String placementId, ImpressionData impressionData) {
+        public void onShowFailure(@NonNull String placementId, ImpressionData impressionData) {
             sendReactEvent("rewardedDidFailToShow", "placementId", placementId);
         }
 
         @Override
-        public void onAvailable(String placementId) {
+        public void onAvailable(@NonNull String placementId) {
             sendReactEvent("rewardedIsAvailable", "placementId", placementId);
         }
 
         @Override
-        public void onUnavailable(String placementId) {
+        public void onUnavailable(@NonNull String placementId) {
             sendReactEvent("rewardedIsUnavailable", "placementId", placementId);
         }
 
         @Override
-        public void onCompletion(@NonNull String placementId, boolean userRewarded) {
+        public void onCompletion(@NonNull @NonNull String placementId, boolean userRewarded) {
             sendReactEvent("rewardedDidComplete", "placementId", placementId);
+        }
+
+        @Override
+        public void onRequestStart(@NonNull String placementId) {
+            sendReactEvent("requestStart", "placementId", placementId);
         }
     };
 
@@ -208,31 +224,83 @@ public class RNFairbidModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void showBannerInView(final String placementId,final String info,final Callback callback) {
-        callback.invoke((Object)null);
-    }
+    public void showBannerInView(final @Nullable ReadableMap options, final Callback callback) {
+        BannerOptions bannerOptions = new BannerOptions();
+        String placementId = options.getString("placementId");
+        Iterator<Map.Entry<String, Object>> n = options.getEntryIterator();
 
-    @ReactMethod
-    public void GDPRCConsent(final boolean consentFlag) {
-        UserInfo.setGdprConsent(consentFlag, getCurrentActivity());
-    }
-    @ReactMethod
-    public void GDPRCConsentData(final @Nullable ReadableMap params) {
-        Map<String, String> consentData = new HashMap<>();
-        if (params != null) {
-            ReadableMapKeySetIterator it = params.keySetIterator();
+        while (n.hasNext()) {
+            Map.Entry<String, Object> key = n.next();
 
-            while(it.hasNextKey()) {
-                String key = it.nextKey();
-
-                consentData.put(key, params.getString(key));
+            switch(key.getKey()) {
+                case "view": //TODO...
+                    break;
+                case "admobBannerSize":
+                    bannerOptions = setAdmobBannersize(bannerOptions, key.getValue());
+                    break;
+                case "facebookBannerSize":
+                    bannerOptions = setFacebookBannersize(bannerOptions, key.getValue());
+                    break;
+                case "fallbackSize":
+                    bannerOptions = setFacebookBannersize(setAdmobBannersize(bannerOptions, key.getValue()), key.getValue());
+                    break;
+                case "placeAtTheBottom":
+                    bannerOptions = bannerOptions.placeAtTheBottom();
+                    break;
+                case "placeAtTheTop":
+                    bannerOptions = bannerOptions.placeAtTheTop();
+                    break;
             }
         }
 
-        UserInfo.setGdprConsentData(consentData, getCurrentActivity());
+        if (placementId != null) {
+            Banner.show(placementId, bannerOptions, getCurrentActivity());
+            
+            callback.invoke(true);
+        } else {
+            callback.invoke(false);
+        }
+
+    }
+
+    private BannerOptions setAdmobBannersize(BannerOptions bannerOptions, Object value) {
+        if (value instanceof String) {
+            switch ((String) value) {
+                case "banner": return bannerOptions.withNetworkSize(SupportedCreativeSizes.ADMOB_BANNER);
+                case "largeBanner": return bannerOptions.withNetworkSize(SupportedCreativeSizes.ADMOB_LARGE_BANNER);
+                case "leaderboard": return bannerOptions.withNetworkSize(SupportedCreativeSizes.ADMOB_LEADERBOARD);
+                case "fullBanner": return bannerOptions.withNetworkSize(SupportedCreativeSizes.ADMOB_FULL_BANNER);
+                case "flexibleWidthPortrait": return bannerOptions.withNetworkSize(SupportedCreativeSizes.ADMOB_SMART_BANNER); //UNSURE
+                case "flexibleWidthLandscape": return bannerOptions.withNetworkSize(SupportedCreativeSizes.ADMOB_LARGE_BANNER); //UNSURE
+            }
+        }
+
+        return bannerOptions;
+    }
+
+    private BannerOptions setFacebookBannersize(BannerOptions bannerOptions, Object value) {
+        if (value instanceof String) {
+            switch ((String) value) {
+                case "flexibleWidthHeight50": return bannerOptions.withNetworkSize(SupportedCreativeSizes.FACEBOOK_BANNER_HEIGHT_50);
+                case "flexibleWidthHeight90": return bannerOptions.withNetworkSize(SupportedCreativeSizes.FACEBOOK_BANNER_HEIGHT_90);
+                case "fb320x50": return bannerOptions.withNetworkSize(SupportedCreativeSizes.FACEBOOK_BANNER_320_50);
+                case "fbRectangle250": return bannerOptions.withNetworkSize(SupportedCreativeSizes.FACEBOOK_BANNER_RECTANGLE_250);
+            }
+        }
+
+        return bannerOptions;
+    }
+
+    @ReactMethod
+    public void GDPRConsent(final boolean consentFlag) {
+        UserInfo.setGdprConsent(consentFlag, getCurrentActivity());
+    }
+    @ReactMethod
+    public void GDPRConsentString(final String consentString) {
+        UserInfo.setGdprConsentString(consentString, getCurrentActivity());
     }
     @ReactMethod
     public void clearGDPRConsent() {
-        UserInfo.clearGdprConsentData(getCurrentActivity());
+        UserInfo.clearGdprConsent(getCurrentActivity());
     }
 }
